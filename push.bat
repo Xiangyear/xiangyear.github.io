25cfa31c2614132df72b3243510ee539dd7cfc1d
Siangus <2687774968@qq.com>
2025-11-08 10:24:10 +0800

    2025-11-08 周六

---
+++ push.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d "%~dp0"
+for /f "tokens=1-4 delims=/: " %%a in ("%date% %time%") do (
+  set datestr=%%a-%%b-%%c %%d
+)
+git add .
+git commit -m "%datestr%"
+git push origin main
+pause
